19bd8b94b61f97cee02fe56d179bfcc858e1db47
megabyte112 <>
2023-01-19 21:41:55 +0000

    add firstboot script

---
+++ firstboot.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This script is run after the installation of the system, after the first boot.
+# Use it to initialise anything that can't be done during installation, such as wallpaper, etc.
+# Don't run any "exit" commands in this script, as this script is designed to self-destruct.
+# This script is NOT run as root. The username is $USER.
+
