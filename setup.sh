782ca4af17db81a25f410bd2d1f6b67a426bc2d4
megabyte112 <>
2023-01-19 21:06:51 +0000

    add comment about chroot

--- setup.sh
+++ setup.sh
@@ -2,6 +2,10 @@
 
 # This script will run after all packages have been installed and files have been copied.
 # The first and only argument passed is the username.
+#
+# Be aware that this does NOT run in the chroot environment
+# To run commands in the chroot environment, use the following syntax:
+#       arch-chroot /mnt bash -c "<your command here>"
 
 # set username variable
 username="$1"
